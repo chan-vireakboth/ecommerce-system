694e42ebf8f489ad384fb719b48ed69ae0f95571
Vireakboth <vireakbothchann@gmail.com>
2026-09-15 11:07:07 +0700

    refactor: remove unused repository and use case interfaces
    delete OrderRepository and CreateOrderUseCase interface files as they are no longer needed

--- order-service/order-service-domain/order-application-service/src/main/java/mptc/gov/kh/ecommerce/domain/port/input/CreateOrderUseCase.java
+++
@@ -1,7 +0,0 @@
-package mptc.gov.kh.ecommerce.domain.port.input;
-
-import mptc.gov.kh.ecommerce.domain.dto.CreateOrderRequest;
-
-public interface CreateOrderUseCase {
-    void execute(CreateOrderRequest createOrderRequest);
-}
--- order-service/order-service-domain/order-application-service/src/main/java/mptc/gov/kh/ecommerce/domain/port/output/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package mptc.gov.kh.ecommerce.domain.port.output;
-
-import mptc.gov.kh.ecommerce.domain.entity.Order;
-
-public interface OrderRepository {
-    Order saveOrder(Order order);
-}
